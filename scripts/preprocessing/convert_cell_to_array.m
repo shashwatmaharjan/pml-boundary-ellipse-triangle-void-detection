1a3c0a2895ef54d045e312a47d94cbda70109731
shashhhw <shashwatmjn@gmail.com>
2024-03-31 18:17:24 -0400

    Updated from signal to displacement

--- scripts/preprocessing/convert_cell_to_array.m
+++ scripts/preprocessing/convert_cell_to_array.m
@@ -12,61 +12,61 @@ converted_data_directory = fullfile(grandparent_directory, 'data', 'converted');
 void_number = 0;
 
 % Load signal data
-signal_data = load(fullfile(raw_data_directory, strcat('void_', string(void_number)), 'signal_data.mat')).signal_data;
+displacement_data = load(fullfile(raw_data_directory, strcat('void_', string(void_number)), 'signal_data.mat')).signal_data;
 
 % Convert the data from cell to matrix
-signal_data = cat(3, signal_data{:});
+displacement_data = cat(3, displacement_data{:});
 
 % Reshape the data such that the shape is: [n_samples, n_time_steps, n_sensors]
-signal_data = permute(signal_data, [3, 2, 1]);
+displacement_data = permute(displacement_data, [3, 2, 1]);
 
 % Load void data
 void_data = load(fullfile(raw_data_directory, strcat('void_', string(void_number)), 'void_data.mat')).Crack_data_yes_no_1;
 
 % Save the signal and void data
-save(fullfile(converted_data_directory, strcat('void_', string(void_number), '_signal_data.mat')), 'signal_data');
+save(fullfile(converted_data_directory, strcat('void_', string(void_number), '_displacement_data.mat')), 'displacement_data');
 save(fullfile(converted_data_directory, strcat('void_', string(void_number), '_void_data.mat')), 'void_data');
 
 %% Void 1
 void_number = 1;
 
 % Load signal data
-signal_data = load(fullfile(raw_data_directory, strcat('void_', string(void_number)), 'signal_data.mat')).signal_data;
+displacement_data = load(fullfile(raw_data_directory, strcat('void_', string(void_number)), 'signal_data.mat')).signal_data;
 
 % Convert sparse cell to regular cell
-signal_data = cellfun(@full, signal_data, 'UniformOutput', false);
+displacement_data = cellfun(@full, displacement_data, 'UniformOutput', false);
 
 % Convert the data from cell to matrix
-signal_data = cat(3, signal_data{:});
+displacement_data = cat(3, displacement_data{:});
 
 % Reshape the data such that the shape is: [n_samples, n_time_steps, n_sensors]
-signal_data = permute(signal_data, [3, 2, 1]);
+displacement_data = permute(displacement_data, [3, 2, 1]);
 
 % Load void data
 void_data = load(fullfile(raw_data_directory, strcat('void_', string(void_number)), 'void_data.mat')).Crack_data_yes_no_1;
 
 % Save the signal and void data
-save(fullfile(converted_data_directory, strcat('void_', string(void_number), '_signal_data.mat')), 'signal_data');
+save(fullfile(converted_data_directory, strcat('void_', string(void_number), '_displacement_data.mat')), 'displacement_data');
 save(fullfile(converted_data_directory, strcat('void_', string(void_number), '_void_data.mat')), 'void_data');
 
 %% Void 2
 void_number = 2;
 
 % Load signal data
-signal_data = load(fullfile(raw_data_directory, strcat('void_', string(void_number)), 'signal_data.mat')).Signal_data;
+displacement_data = load(fullfile(raw_data_directory, strcat('void_', string(void_number)), 'signal_data.mat')).Signal_data;
 
 % Convert sparse cell to regular cell
-signal_data = cellfun(@full, signal_data, 'UniformOutput', false);
+displacement_data = cellfun(@full, displacement_data, 'UniformOutput', false);
 
 % Convert the data from cell to matrix
-signal_data = cat(3, signal_data{:});
+displacement_data = cat(3, displacement_data{:});
 
 % Reshape the data such that the shape is: [n_samples, n_time_steps, n_sensors]
-signal_data = permute(signal_data, [3, 2, 1]);
+displacement_data = permute(displacement_data, [3, 2, 1]);
 
 % Load void data
 void_data = load(fullfile(raw_data_directory, strcat('void_', string(void_number)), 'void_data.mat')).Crack_data_yes_no_1;
 
 % Save the signal and void data
-save(fullfile(converted_data_directory, strcat('void_', string(void_number), '_signal_data.mat')), 'signal_data');
+save(fullfile(converted_data_directory, strcat('void_', string(void_number), '_displacement_data.mat')), 'displacement_data');
 save(fullfile(converted_data_directory, strcat('void_', string(void_number), '_void_data.mat')), 'void_data');
